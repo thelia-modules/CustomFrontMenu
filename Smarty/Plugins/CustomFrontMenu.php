2c7a0b52afa435142c026c418a3c3b924854db65
NoaSlld <noa.sillard@gmail.com>
2024-05-24 12:05:55 +0200

    use now an adaptative path

--- Smarty/Plugins/CustomFrontMenu.php
+++ Smarty/Plugins/CustomFrontMenu.php
@@ -8,31 +8,6 @@ use CustomFrontMenu\Controller\MenuController;
 
 class CustomFrontMenu extends AbstractSmartyPlugin
 {
-    /*
-    public function getPluginDescriptors(): array
-    {
-        return [
-            new SmartyPluginDescriptor(
-                'function',
-                'CustomFrontMenu',
-                $this,
-                'CustomFrontMenu'
-            ),
-        ];
-    }
-    
-    public function CustomFrontMenu($params): void // dependency injection of the custom front menu service
-    {
-        if (!isset($params['menu_id'])) {
-            throw new \InvalidArgumentException('The menu_id parameter is required');
-        }
-        // $menu = $customFrontMenuService->getMenu($params['menu_id']); // Waiting for the custom front menu service
-        $ctrl = new MenuController;
-
-        $this->render("customFrontMenu.html", ['menuItems' => $ctrl->getMenuItems()]);
-    }
-    */
-
     public function getPluginDescriptors(): array
     {
         return [
@@ -55,8 +30,7 @@ class CustomFrontMenu extends AbstractSmartyPlugin
 
         $menuItems = $ctrl->getMenuItems();
         $smarty->assign('menuItems', $menuItems);
-        //change by the racine of the project
-        $templatePath = "/Users/nsillard/Sites/thelia-modules/local/modules/CustomFrontMenu/templates/frontOffice/default/customFrontMenu.html";
+        $templatePath = THELIA_LOCAL_DIR . '/modules/CustomFrontMenu/templates/frontOffice/default/customFrontMenu.html';
         $smarty->display($templatePath);
     }
 }
