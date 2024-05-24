6b0767ab95f565c6e26bbd52d52a0d81a1e1286f
NoaSlld <noa.sillard@gmail.com>
2024-05-24 11:51:56 +0200

    smarty plugin work, need to change the path

--- Smarty/Plugins/CustomFrontMenu.php
+++ Smarty/Plugins/CustomFrontMenu.php
@@ -8,6 +8,7 @@ use CustomFrontMenu\Controller\MenuController;
 
 class CustomFrontMenu extends AbstractSmartyPlugin
 {
+    /*
     public function getPluginDescriptors(): array
     {
         return [
@@ -29,5 +30,33 @@ class CustomFrontMenu extends AbstractSmartyPlugin
         $ctrl = new MenuController;
 
         $this->render("customFrontMenu.html", ['menuItems' => $ctrl->getMenuItems()]);
+    }
+    */
+
+    public function getPluginDescriptors(): array
+    {
+        return [
+            new SmartyPluginDescriptor(
+                'function',
+                'CustomFrontMenu',
+                $this,
+                'renderCustomFrontMenu'
+            ),
+        ];
+    }
+
+    public function renderCustomFrontMenu($params, $smarty): void
+    {
+        if (!isset($params['menu_id'])) {
+            throw new \InvalidArgumentException('The menu_id parameter is required', 0);
+        }
+
+        $ctrl = new MenuController();
+
+        $menuItems = $ctrl->getMenuItems();
+        $smarty->assign('menuItems', $menuItems);
+        //change by the racine of the project
+        $templatePath = "/Users/nsillard/Sites/thelia-modules/local/modules/CustomFrontMenu/templates/frontOffice/default/customFrontMenu.html";
+        $smarty->display($templatePath);
     }
 }
