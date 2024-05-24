38aeb7bc71194c52338810e98e62c723627f4374
NoaSlld <noa.sillard@gmail.com>
2024-05-24 15:31:57 +0200

    doesn't work

--- Hook/ConfigHook.php
+++ Hook/ConfigHook.php
@@ -2,19 +2,27 @@
 
 namespace CustomFrontMenu\Hook;
 
-use Thelia\Core\Event\Hook\HookRenderEvent;
-use Thelia\Core\Hook\BaseHook;
 use CustomFrontMenu\Controller\MenuController;
+use CustomFrontMenu\Interface\CFMSaveInterface;
+use CustomFrontMenu\Interface\CFMLoadInterface;
+use Thelia\Core\Hook\BaseHook;
+use Thelia\Core\Event\Hook\HookRenderEvent;
 
 class ConfigHook extends BaseHook
 {
-    public function onModuleConfiguration(HookRenderEvent $event)
+    protected $cfmSave, $cfmLoad, $menuController;
+
+    public function __construct(CFMSaveInterface $cfmSave, CFMLoadInterface $cfmLoad, MenuController $menuController)
     {
+        parent::__construct();
+        $this->cfmSave = $cfmSave;
+        $this->cfmLoad = $cfmLoad;
+        $this->menuController = $menuController;
+    }
 
-        $controller = new MenuController;
-
-        $controller->loadMenuItems();
-
+    public function onModuleConfiguration(HookRenderEvent $event): void
+    {
+        $this->menuController->loadMenuItems();
         $event->add($this->render("module-config.html"));
     }
 }
