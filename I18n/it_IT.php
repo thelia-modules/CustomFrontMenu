954e1bc6f79d08f37aff2dd4314b37380eb6419a
mnordest <matthiasnordest@gmail.com>
2024-06-07 10:51:02 +0200

    add es and it translations

---
+++ I18n/it_IT.php
@@ -0,0 +1,23 @@
+<?php
+
+return array(
+    'Brand' => 'Marca',
+    'Category' => 'Categoria',
+    'Product' => 'Prodotto',
+    'Content' => 'Contenuto',
+    'Folder' => 'Cartella',
+    'Link to' => 'Collegamento a',
+    'Menu name' => 'Nome del menu',
+    'New link' => 'Nuovo collegamento',
+    'No parent' => 'Nessun genitore',
+    'Page' => 'Pagina',
+    'Parent' => 'Genitore',
+    'URL' => 'URL',
+
+    'This menu does not exists' => 'Questo menu non esiste',
+
+    'Current menu deleted successfully' => 'Il menu è stato eliminato con successo',
+    'New menu added successfully' => 'Nuovo menu aggiunto con successo',
+    'This menu has been successfully saved !' => 'Questo menu è stato salvato con successo!'
+
+);
