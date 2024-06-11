cc0a8c1bbc073e32efee2ab1215c12f8cd1364c2
mathisdlg <delage.mathis.1@gmail.com>
2024-06-11 14:11:34 +0200

    Refactor CustomFrontMenuPlugin to use RequestStack instead of Session

--- Smarty/Plugins/CustomFrontMenuPlugin.php
+++ Smarty/Plugins/CustomFrontMenuPlugin.php
@@ -3,12 +3,9 @@ namespace CustomFrontMenu\Smarty\Plugins;
 
 use CustomFrontMenu\Service\CustomFrontMenuLoadService;
 use CustomFrontMenu\Service\CustomFrontMenuService;
-use Thelia\Core\Event\TheliaEvents;
-use Thelia\Core\Template\TemplateHelperInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 use TheliaSmarty\Template\AbstractSmartyPlugin;
 use TheliaSmarty\Template\SmartyPluginDescriptor;
-use Thelia\Core\HttpFoundation\Session\Session;
-use Symfony\Component\Cache\Adapter\AdapterInterface;
 
 class CustomFrontMenuPlugin extends AbstractSmartyPlugin
 {
@@ -16,7 +13,7 @@ class CustomFrontMenuPlugin extends AbstractSmartyPlugin
     public function __construct(
         private CustomFrontMenuLoadService $CustomFrontMenuLoadService,
         private CustomFrontMenuService $customFrontMenuService,
-        private Session $session
+        private RequestStack $requestStack
     ) {
     }
 
@@ -34,16 +31,18 @@ class CustomFrontMenuPlugin extends AbstractSmartyPlugin
 
     public function renderCustomFrontMenuPlugin($params, $smarty): void
     {
-        if (!isset($params['menu_id'])) {
+        $langAdmin = $this->requestStack->getCurrentRequest()->getSession()->getAdminLang()->getLocale();
+
+        if (!$params['menu_id']) {
             throw new \InvalidArgumentException('The menu_id parameter is required', 1);
         }
 
         $menu = $this->customFrontMenuService->getMenu($params['menu_id']);
-        if (!isset($menu)) {
+        if (!$menu) {
             throw new \InvalidArgumentException('The menu does not exist', 2);
         }
 
-        $menuItems = $this->CustomFrontMenuLoadService->loadTableBrowserLang($menu, $this->session->get('thelia.current.lang')->getLocale());
+        $menuItems = $this->CustomFrontMenuLoadService->loadTableBrowserLang($menu, $langAdmin);
         $smarty->assign('menuItems', $menuItems);
 
         $cssPath = $smarty->getTemplateDir("CustomFrontMenu"). "assets/css/customFrontMenu.css.html";
