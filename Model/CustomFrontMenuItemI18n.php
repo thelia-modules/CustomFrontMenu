928e89d4279280d6a056fb1782d0cfba912aff9f
mnordest <matthiasnordest@gmail.com>
2024-06-14 16:02:12 +0200

    Repair php storm bad delete

---
+++ Model/CustomFrontMenuItemI18n.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace CustomFrontMenu\Model;
+
+use CustomFrontMenu\Model\Base\CustomFrontMenuItemI18n as BaseCustomFrontMenuItemI18n;
+
+/**
+ * Skeleton subclass for representing a row from the 'custom_front_menu_item_i18n' table.
+ *
+ *
+ *
+ * You should add additional methods to this class to meet the
+ * application requirements.  This class will only be generated as
+ * long as it does not already exist in the output directory.
+ */
+class CustomFrontMenuItemI18n extends BaseCustomFrontMenuItemI18n
+{
+
+}
